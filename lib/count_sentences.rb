9ec83bc0807cc6978a7cf6cc2b0d29ac8a6c23dc
Hayley Howell <hayley_howell@sbcglobal.net>
2018-06-07 14:57:20 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    period = self.split(".")
+    question = self.split("?")
+    exclaim = self.split("!")
+    period.count + question.count + exclaim.count
   end
 end
